25846b3e08b2151222485829db44872de1b95fbc
vcabrera575 <vcabrera575@gmail.com>
2020-11-03 02:50:21 -0700

    Added rope model for player to eventually swing on. Mouse click now makes rope appear.

---
+++ Hookjet/Assets/Scripts/PlayerShoot.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerShoot : MonoBehaviour
+{
+    public GameObject rope;
+
+    float distance = 2f;
+    public float speed = 20f;
+
+    GameObject newRope;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            Shoot();
+        }
+        else if(Input.GetButtonUp("Fire1"))
+        {
+            Destroy(newRope);
+        }
+    }
+
+    void Shoot()
+    {
+        RaycastHit hit;
+        if (Physics.Raycast(transform.position, transform.forward, out hit))
+        {
+            if (hit.transform.tag == "Environment")
+            {
+                // This is where we create things when we hit an environment object
+                newRope = Instantiate(rope, transform.position, transform.rotation);
+
+                //newRope.transform.rotation = direction;
+            }
+        }
+
+    }
+}
