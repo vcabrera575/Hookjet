5a0d4a5e9711e5a9a025fdbf18dad1bc51b6da81
vcabrera575 <vcabrera575@gmail.com>
2020-11-03 00:06:26 -0700

    Added first player movement, and a base environment to play around in

---
+++ Hookjet/Assets/Scripts/GameController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour
+{
+    // Timer management
+    float gameStartTime = 0f;
+
+    // Player management
+    public float playerSpeed = 12f;
+    public float playerBaseSpeed = 12f;
+
+    public float playerFullness = 0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+
+        // Increment timer
+        gameStartTime += Time.deltaTime;
+    }
+}
---
+++ Hookjet/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    Vector3 velocity;
+    public CharacterController playerController;
+    public GameController gameController;
+
+    public float gravity = -9.81f;
+    public float jumpHeight = 3f;
+
+    bool grounded;
+
+    // Update is called once per frame
+    void Update()
+    {
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+        float deadZone = 0.25f; // Controller deadzone.
+
+        Vector3 move = transform.right * x + transform.forward * z;
+
+        // This will fix deadzone and normalization on controllers as well as keyboard.
+        if (move.magnitude < deadZone)
+            move = Vector3.zero;
+        else
+            move = move.normalized * ((move.magnitude - deadZone) / (1 - deadZone));
+
+        // Check if the player is on the ground
+        if (playerController.isGrounded && velocity.y < 0)
+            grounded = true;
+        else
+            grounded = false;
+
+        // Gravity
+        if (grounded && velocity.y < 0)
+        {
+            velocity.y = -2f;
+        }
+
+        // Detects if the player is pressing jump key
+        if (Input.GetButtonDown("Jump") && grounded)
+        {
+            velocity.y = Mathf.Sqrt(jumpHeight * -2 * gravity);
+            grounded = false;
+        }
+
+        velocity.y += gravity * Time.deltaTime;
+
+        playerController.Move((move * gameController.playerSpeed * Time.deltaTime) + (velocity * Time.deltaTime));
+    }
+}
