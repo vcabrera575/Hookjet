1054d9f40c02f81c0243879c6e5ee79a751ec94a
vcabrera575 <vcabrera575@gmail.com>
2020-11-16 18:13:50 -0700

    Switch from 3D to 2D

--- Hookjet/Assets/Scripts/PlayerMovement.cs
+++ Hookjet/Assets/Scripts/PlayerMovement.cs
@@ -7,15 +7,18 @@ public class PlayerMovement : MonoBehaviour
 {
     Vector3 velocity;
     //public CharacterController playerController;
-    Rigidbody playerController;
+    Rigidbody2D playerController;
     public GameController gameController;
     Vector3 inputs;
 
     public float gravity = -9.81f;
     public float hookGravity = -6.81f;
     public float jumpHeight = 3f;
-    public float acceleration = 0f;
+    public float acceleration = 0f; // Acceleration while the player is on a hook
     float playerMass;
+
+    // Velocity
+    Vector3 currentVelocity = Vector3.zero;
 
     // Variables for the ground
     public Transform groundCheck;
@@ -29,6 +32,7 @@ public class PlayerMovement : MonoBehaviour
     // Time Management
     float dashTimer = 0f;
 
+    Collider2D[] groundColliders;
     bool grounded;
     bool dashCooldown;
     bool wasOnHook;
@@ -37,38 +41,90 @@ public class PlayerMovement : MonoBehaviour
     {
         dashCooldown = false;
         wasOnHook = false;
-        playerController = GetComponent<Rigidbody>();
+        playerController = GetComponent<Rigidbody2D>();
         playerMass = playerController.mass;
     }
 
     void Update()
     {
+        inputs = Vector2.zero;
+        inputs.x = Input.GetAxis("Horizontal");
+        inputs.y = Input.GetAxis("Vertical");
+
+        float deadZone = 0.025f; // Controller deadzone
+
+        // Fix deadzone for controllers and add normalization
+        if (inputs.magnitude < deadZone)
+            inputs = Vector2.zero;
+        else
+            inputs = inputs.normalized * ((inputs.magnitude - deadZone) / (1 - deadZone));
+
+        if (!grounded)
+            inputs.y = playerController.velocity.y;
+        else
+        {
+            inputs.y = 0;
+            Debug.Log(grounded);
+        }
+    }
+
+    void FixedUpdate()
+    {
+
+        bool wasGrounded = grounded;
+        grounded = false;
+
+        // The player is grounded if a circlecast to the groundcheck position hits anything designated as ground
+        // This can be done using layers instead but Sample Assets will not overwrite your project settings.
+        Collider2D[] groundColliders = Physics2D.OverlapCircleAll(groundCheck.position, groundDistance, groundLayer);
+
+        for (int i = 0; i < groundColliders.Length; i++)
+        {
+            if (groundColliders[i].gameObject != gameObject)
+            {
+                grounded = true;
+            }
+        }
+
+        float movementSpeed = gameController.playerSpeed * acceleration * Time.fixedDeltaTime;
+
+        // Move the character by finding the target velocity
+        Vector3 targetVelocity = new Vector2( inputs.x * movementSpeed, playerController.velocity.y);
+        // And then smoothing it out and applying it to the character
+        playerController.velocity = targetVelocity;
+    }
+
+    void OldUpdate()
+    {
         //grounded = Physics.CheckSphere(groundCheck.position, GroundDistance, groundCheck, QueryTriggerInteraction.Ignore); 
         inputs = Vector3.zero;
         inputs.x = Input.GetAxis("Horizontal");
-        inputs.z = Input.GetAxis("Vertical");
+        inputs.y = Input.GetAxis("Vertical");
         float deadZone = 0.25f; // Controller deadzone.
 
         // This will fix deadzone and normalization on controllers as well as keyboard.
         if (inputs.magnitude < deadZone)
-            inputs = Vector3.zero;
+            inputs = Vector2.zero;
         else
             inputs = inputs.normalized * ((inputs.magnitude - deadZone) / (1 - deadZone));
 
         grounded = Physics.CheckSphere(groundCheck.transform.position, groundDistance, groundLayer);
 
+        if (grounded)
+            Debug.Log("test");
+
         // Player jump
         if (Input.GetButtonDown("Jump") && grounded)
         {
-            playerController.AddForce(Vector3.up * Mathf.Sqrt(jumpHeight * -2f * Physics.gravity.y), ForceMode.VelocityChange);
+            playerController.AddForce(Vector2.up * Mathf.Sqrt(jumpHeight * -2f * Physics.gravity.y));
             grounded = false;
         }
 
         // Player dashing
         if (Input.GetButtonDown("Dash") && !dashCooldown)
         {
-            Vector3 dashVelocity = (gameController.dashDistance * transform.forward);
-            playerController.AddForce(dashVelocity, ForceMode.VelocityChange);
+            Vector3 dashVelocity = (gameController.dashDistance * transform.right);
+            playerController.AddForce(dashVelocity);
             dashCooldown = true;
             dashTimer = gameController.dashResetTime;
         }
@@ -114,18 +170,18 @@ public class PlayerMovement : MonoBehaviour
         acceleration = 1f;
     }
 
-    void FixedUpdate()
+    void OldFixedUpdate()
     {
         //playerController.MovePosition(playerController.position + inputs * gameController.playerSpeed * Time.fixedDeltaTime);
         float movementSpeed = gameController.playerSpeed * acceleration * Time.deltaTime;
 
         float distance = inputs.magnitude * Time.fixedDeltaTime;
-        RaycastHit hit;
-        if (playerController.SweepTest(inputs * acceleration * movementSpeed, out hit, distance))
-            playerController.velocity = new Vector3(0, playerController.velocity.y, 0);
+        //RaycastHit hit;
+        //if (playerController.SweepTest(inputs * acceleration * movementSpeed, out hit, distance))
+        //    playerController.velocity = new Vector3(0, playerController.velocity.y, 0);
         
         
-        playerController.MovePosition(transform.position + (transform.forward * inputs.z * movementSpeed) + (transform.right * inputs.x * movementSpeed));
+        playerController.MovePosition(transform.position + (transform.up * inputs.y * movementSpeed) + (transform.right * inputs.x * movementSpeed));
 
     }
 
